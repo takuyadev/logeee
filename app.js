605fd78d1373c261408900955d7140a919ef3b44
= <emma@emmas-mbp.wifi.bcit.ca>
2020-03-02 15:01:47 -0800

    use to get button css

---
+++ app.js
@@ -0,0 +1,9 @@
+function Page2() {
+    document.querySelector("#page1Content").style.display = "none";
+    document.querySelector("#page2Content").style.display = "flex";
+}
+
+function Page3() {
+    document.querySelector("#page2Content").style.display = "none";
+    document.querySelector("#page3Content").style.display = "flex";
+}
